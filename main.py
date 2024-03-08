5c206913abdff88f8828b7158120e7548ac6a395
sistemasinfo2024 <161856932+sistemasinfo2024@users.noreply.github.com>
2024-03-08 11:30:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
+pb = pd.read_csv('publishers.csv' )  
+print(type(pb.to_dict()))
+
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hola Grupo"}
+<<<<<<< HEAD
+async def root() -> List[Dict]:
+    pb = pd.read_csv('publishers.csv')    
+    # list(pb.to_dict())
+    return pb.iloc[:10,:].to_dict(orient='records')
+    # return {"message": "Hola Mundo!!!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
